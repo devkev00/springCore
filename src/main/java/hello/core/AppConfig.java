e0f6afad1d4c02176850572286457b2757b071a1
devkev00 <kimminsoo116@gmail.com>
2024-10-01 22:55:10 +0900

    feat: Benefit of DIP

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,6 +10,7 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
@@ -23,7 +24,10 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // return new FixDiscountPolicy();
+        // DIP를 지키면 이렇게 클라이언트 코드를 하나도 변경하지 않고도 로직을 변경할 수 있다.
+        // 이는 OCP 또한 지키는 셈이다.
+        return new RateDiscountPolicy();
     }
 
 }
