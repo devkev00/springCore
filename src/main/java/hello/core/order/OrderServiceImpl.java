a4b0cb708d3964d8221ee9cc0fdec6c99e3b0cb8
devkev00 <kimminsoo116@gmail.com>
2024-10-01 18:47:39 +0900

    feat: DIP policy problem

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +9,11 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // DIP 원칙 위반, 인터페이스뿐이 아닌 구현체에도 의존하고 있다.
+    private DiscountPolicy discountPolicy;
+    // 이렇게 하면 DIP 원칙은 지켜지지만, 구현체가 없으므로 NPE가 발생
+    // 따라서 의존성을 따로 주입해주는 역할을 하는 '누군가' 가 필요하다!
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
