9f72bfa7a71ed456a98bd5d556b0caf19b81996f
devkev00 <kimminsoo116@gmail.com>
2024-10-30 16:27:32 +0900

    feat: Singleton & @Configuration

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,16 +16,19 @@ public class AppConfig {
 
     @Bean // 스프링 컨테이너에 등록하는 어노테이션
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
