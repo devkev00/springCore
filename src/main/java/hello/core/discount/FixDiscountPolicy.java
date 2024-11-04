1be74766b045438df7d5ab29f1c4e3ab2961a2f0
devkev00 <kimminsoo116@gmail.com>
2024-11-04 23:23:25 +0900

    feat: If beans are more than 2

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component 
+// 현재 Component 어노테이션을 지정 -> AutoAppConfig의 로직 때문에 의존관계 자동 주입 -> Bean이 두 개 등록되는 문제 발생
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 천원 할인
