f1d78ea2b2d92f1587534bf164feb1e9add26d1d
devkev00 <kimminsoo116@gmail.com>
2024-11-05 00:01:45 +0900

    feat: Making annotation

---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,13 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy") // Qualifier 역할을 하는 어노테이션을 직접 만들었다.
+public @interface MainDiscountPolicy {
+}
